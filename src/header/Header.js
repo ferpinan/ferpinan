bbb5b9cbb0f138126738ba202eb246783d6ae8c2
andonifernandez <andonifernandez@paradigmadigital.com>
2022-02-21 13:06:43 +0100

    converted page to react

---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+const Header = () => (
+	<div className={"header"}>
+		<h1>Andoni Fernandez Pina</h1>
+		<h3>Software Engineer | Web developer | a.fernandez.pina@gmail.com</h3>
+	</div>
+);
+
+export default Header;
